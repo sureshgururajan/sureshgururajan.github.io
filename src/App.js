d1c4c1ad72a3c65f2a2e8d132ebac2df282bf215
Suresh Gururajan <gusuresh@amazon.com>
2020-12-30 20:51:47 -0800

    minor change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Edited by Suresh
+          Edited by Suresh again
         </p>
         <a
           className="App-link"
