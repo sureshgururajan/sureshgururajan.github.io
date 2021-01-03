274a742cf6c31fd2362107b0d3253ed6ca326867
Suresh Gururajan <sureshgururajan@gmail.com>
2021-01-03 10:35:55 -0800

    latest deployment

--- src/Posts.js
+++ src/Posts.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Post from './Post';
+
+const postData = require("./post_data.json");
+
+function PostList(data) {
+    let postcontents = postData["posts"];
+    const postItems = postcontents.map((item) =>
+        <li>{<Post title={item["title"]} />}</li>
+    );
+    return (
+        <ul>
+            {postItems}
+        </ul>
+    );
+}
 
 function Posts() {
-  return (
-    <div> Posts </div>
-  )
+    return (
+        <div> {PostList(postData)} </div>
+    )
 }
 
 export default Posts;
